9db4b4445777d5cf986368870481baa5b44e7f6c
Neotwalker <and1marat9@gmail.com>
2023-02-02 14:51:24 +0500

    add

--- js/common.js
+++ js/common.js
@@ -60,4 +60,11 @@ $(function() {
 		});
 	}
 
+	$("#menu, #menu1").on("click","a", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+	$('body,html').animate({scrollTop: top}, 1000);
+	});
+
 });
